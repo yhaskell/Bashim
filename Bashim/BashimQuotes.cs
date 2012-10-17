dcd55e2907ad8725b9c24ea0fe93682ab559a793
Igor Dultsev <yhaskell@live.com>
2012-10-17 08:14:07 +0700

    Overall commit - version 1.0

---
+++ Bashim/BashimQuotes.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Bashim
+{
+    public class BashOrgRu
+    {
+        public class Quote
+        {
+            public string id { get; set; }
+            public string quote { get; set; }
+        }
+
+        static string Convert(string s)
+        {
+            return s.Replace("&gt;", ">").Replace("&lt;", "<").Replace("&nbsp;", " ").Replace("&amp;", "&").Replace("&quot;", "\"");
+        }
+
+        IEnumerable<Quote> Quotes(string s, bool force = false)
+        {
+            int index = s.IndexOf("<a href=\"/quote/");
+            if (force) index = 1;
+            while (index != -1)
+            {
+
+                string iNum = s.Substring(index + 16, 6);
+
+                int qstart = s.IndexOf("<div class=\"text\">", index);
+                int qend = s.IndexOf("</div>", qstart);
+
+                string quote = s.Substring(qstart + 18, qend - qstart - 18).Replace("<br>", "\n").Replace("<br />", "\n");
+
+                yield return new Quote { id = iNum, quote = Convert(quote) };
+
+                index = s.IndexOf("<a href=\"/quote/", qend);
+            }
+            yield break;
+        }
+
+        IEnumerable<Quote> AbyssTopQuotes(string s)
+        {
+            int index = 0;
+            int num = 1;
+            while (index != -1)
+            {                
+                int qstart = s.IndexOf("<div class=\"text\">", index);
+                int qend = s.IndexOf("</div>", qstart);
+
+                string quote = s.Substring(qstart + 18, qend - qstart - 18).Replace("<br>", "\n").Replace("<br />", "\n");
+
+                yield return new Quote { id = (num++).ToString(), quote = Convert(quote) };
+
+                index = s.IndexOf("<div class=\"quote", qend);
+            }
+            yield break;
+        }
+
+        public int PagesCount { get; set; }
+
+        public async Task<IEnumerable<Quote>> GetNewQuotes(int pageId = -1)
+        {
+            HttpClient http = new HttpClient();
+
+            var link = pageId >0 ? "http://bash.im/index/" + pageId.ToString() : "http://bash.im/";
+
+            var httpResult = await http.GetAsync(link);
+
+            IEnumerable<Quote> result = null;
+
+            var enc = Encoding.GetEncoding("windows-1251");
+            var byteArray = await httpResult.Content.ReadAsByteArrayAsync();
+            var d = enc.GetString(byteArray, 0, byteArray.Length).Substring(16);
+
+            Regex regex = new Regex(@"min=""1"" max=""(\d*)"" value=""(\d*)""");
+
+            var u = regex.Match(d);
+
+            var max = u.Groups[1].Value;
+            var value = u.Groups[2].Value;
+
+            PagesCount = int.Parse(max);
+
+            var begin = d.IndexOf("<div class=\"quote\">");
+            var end = d.IndexOf("<div class=\"pager\">", begin);
+
+            var s = d.Substring(begin, end - begin);
+
+            result = Quotes(s);
+
+            return result;
+        }
+
+        private int? nextRandomSeed;
+
+        public async Task<IEnumerable<Quote>> GetRandomQuotes()
+        {
+            HttpClient http = new HttpClient();
+
+            var link = nextRandomSeed != null ? "http://bash.im/random?" + nextRandomSeed.ToString() : "http://bash.im/random";
+
+            var httpResult = await http.GetAsync(link);
+
+            IEnumerable<Quote> result = null;
+
+            var enc = Encoding.GetEncoding("windows-1251");
+            var byteArray = await httpResult.Content.ReadAsByteArrayAsync();
+            var d = enc.GetString(byteArray, 0, byteArray.Length).Substring(16);
+
+            var begin = d.IndexOf("<div class=\"quote\">");
+            var end = d.IndexOf(@"<a href=""/random", begin);
+
+            var s = d.Substring(begin, end - begin);
+
+            Regex regex = new Regex(@"/random\?(\d*)");
+
+            var u = regex.Match(d);
+
+            var seed = u.Groups[1].Value;
+            nextRandomSeed = int.Parse(seed);
+
+            result = Quotes(s);
+
+            return result;
+        }
+
+        public int? nextAbyssSeed;
+
+        public async Task<IEnumerable<Quote>> GetAbyss()
+        {
+            HttpClient http = new HttpClient();
+
+            var link = nextAbyssSeed != null ? "http://bash.im/abyss?" + nextRandomSeed.ToString() : "http://bash.im/abyss";
+
+            var httpResult = await http.GetAsync(link);
+
+            IEnumerable<Quote> result = null;
+
+            var enc = Encoding.GetEncoding("windows-1251");
+            var byteArray = await httpResult.Content.ReadAsByteArrayAsync();
+            var d = enc.GetString(byteArray, 0, byteArray.Length).Substring(16);
+
+            var begin = d.IndexOf("<div class=\"quote\">");
+            var end = d.IndexOf("<div class=\"quote more\">", begin);
+
+            var s = d.Substring(begin, end - begin);
+
+            Regex regex = new Regex(@"/abyss\?(\d*)");
+
+            var u = regex.Match(d);
+
+            var seed = u.Groups[1].Value;
+            nextAbyssSeed = int.Parse(seed);
+
+            result = Quotes(s);
+
+            return result;
+        }
+
+        public async Task<IEnumerable<Quote>> GetAbyssTop()
+        {
+            HttpClient http = new HttpClient();
+
+            var link = "http://bash.im/abysstop";
+
+            var httpResult = await http.GetAsync(link);
+
+            IEnumerable<Quote> result = null;
+
+            var enc = Encoding.GetEncoding("windows-1251");
+            var byteArray = await httpResult.Content.ReadAsByteArrayAsync();
+            var d = enc.GetString(byteArray, 0, byteArray.Length).Substring(16);
+
+            var begin = d.IndexOf("<div class=\"quote\">");
+            var end = d.IndexOf("<div class=\"inside\">", begin);
+
+            var s = d.Substring(begin, end - begin);
+            
+            result = AbyssTopQuotes(s);
+
+            return result;
+        }
+    }
+}
---
+++ Bashim/MainPage.xaml.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.ApplicationSettings;
+using Windows.UI.Popups;
+using Windows.System;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace Bashim
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class MainPage : Bashim.Common.LayoutAwarePage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SettingsPane.GetForCurrentView().CommandsRequested += SettingCharmManager_CommandsRequested;
+        }
+
+        private void SettingCharmManager_CommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        {
+            args.Request.ApplicationCommands.Add(new SettingsCommand("privacypolicy", "Privacy policy", OpenPrivacyPolicy));
+        }
+
+        private async void OpenPrivacyPolicy(IUICommand command)
+        {
+            Uri uri = new Uri("http://wintheweb.ru/openprivacy.html");
+            await Launcher.LaunchUriAsync(uri);
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="pageState">An empty dictionary to be populated with serializable state.</param>
+        protected override void SaveState(Dictionary<String, Object> pageState)
+        {
+        }
+
+        private void pageRoot_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            QuotesSelector.SelectedIndex = 0;
+        }
+
+        bool SelectionMode = false;
+
+        private void QS_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SelectionMode == true) return;
+            SelectionMode = true;
+
+            AbyssSelector.SelectedIndex = -1;
+            MoarLove.Visibility = Visibility.Visible;
+            switch (QuotesSelector.SelectedIndex)
+            {
+                case 0:
+                    RemoveQuotes();
+                    LoadNewQuotes();
+                    break;
+                case 1:
+                    RemoveQuotes();
+                    LoadRandomQuotes();
+                    break;
+            }
+
+
+            SelectionMode = false;
+        }
+
+        private void AS_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SelectionMode == true) return;
+            SelectionMode = true;
+
+            QuotesSelector.SelectedIndex = -1;
+
+            switch (AbyssSelector.SelectedIndex)
+            {
+                case 0:
+                    MoarLove.Visibility = Visibility.Visible;
+                    RemoveQuotes();
+                    LoadAbyss();
+                    break;
+                case 1:
+                    MoarLove.Visibility = Visibility.Collapsed;
+                    RemoveQuotes();
+                    LoadTopAbyss();                    
+                    break;
+            }
+
+            SelectionMode = false;
+        }
+
+        BashOrgRu bashQuotes = new BashOrgRu();
+
+        FontFamily Consolas = new FontFamily("Consolas");
+
+        int lastPageLoaded;
+
+        SolidColorBrush lgb = new SolidColorBrush(Windows.UI.ColorHelper.FromArgb(255, 241, 241, 241));
+
+        Thickness Twelve = new Thickness(12);
+
+        void AddQuote(string quote)
+        {
+            TextBlock t = new TextBlock { Width = 600, TextWrapping = TextWrapping.Wrap, HorizontalAlignment = HorizontalAlignment.Left, FontSize = 14, Text = quote, Margin = Twelve, FontFamily = Consolas };
+            TextBlock t2 = new TextBlock { Width = 270, TextWrapping = TextWrapping.Wrap, HorizontalAlignment = HorizontalAlignment.Left, FontSize = 12, Text = quote, Margin = Twelve, FontFamily = Consolas };
+            QuotesViewer.Children.Insert(QuotesViewer.Children.Count - 1, new Border { Background = lgb, Margin = Twelve, Child = t, Width = 800 });
+            QuotesViewerSnapped.Children.Insert(QuotesViewerSnapped.Children.Count - 1, new Border { Background = lgb, Margin = Twelve, Child = t2, Width = 350 });
+        }
+
+        private async void LoadNewQuotes(bool More = false)
+        {
+            try
+            {
+                if (More == true)
+                {
+                    var q = await bashQuotes.GetNewQuotes(--lastPageLoaded);
+
+                    foreach (var quote in q)
+                        AddQuote(quote.quote);
+
+                    return;
+                }
+
+                lastPageLoaded = bashQuotes.PagesCount;
+                foreach (var quote in await bashQuotes.GetNewQuotes()) AddQuote(quote.quote);
+            }
+            catch
+            {
+                new MessageDialog("Во время получения новых цитат произошла какая-то ошибка. Возможно, сломались интернеты. Если данная ошибка повторится, обратитесь к разработчику.", "Bash.im").ShowAsync();
+            }
+        }
+
+        private async void LoadRandomQuotes()
+        {
+            try
+            {
+                var RandomQuotes = await bashQuotes.GetRandomQuotes();
+
+                foreach (var quote in RandomQuotes)
+                    AddQuote(quote.quote);
+            }
+            catch
+            {
+                new MessageDialog("Во время получения новых цитат произошла какая-то ошибка. Возможно, сломались интернеты. Если данная ошибка повторится, обратитесь к разработчику.", "Bash.im").ShowAsync();
+            }
+        }
+
+        private async void LoadAbyss()
+        {
+            try
+            {
+                var AbyssQuotes = await bashQuotes.GetAbyss();
+
+                foreach (var quote in AbyssQuotes)
+                    AddQuote(quote.quote);
+            }
+            catch
+            {
+                new MessageDialog("Во время получения новых цитат произошла какая-то ошибка. Возможно, сломались интернеты. Если данная ошибка повторится, обратитесь к разработчику.", "Bash.im").ShowAsync();
+            }
+        }
+
+        private async void LoadTopAbyss()
+        {
+            try
+            {
+                var AbyssQuotes = await bashQuotes.GetAbyssTop();
+
+                foreach (var quote in AbyssQuotes)
+                    AddQuote(quote.quote);
+            }
+            catch
+            {
+                new MessageDialog("Во время получения новых цитат произошла какая-то ошибка. Возможно, сломались интернеты. Если данная ошибка повторится, обратитесь к разработчику.", "Bash.im").ShowAsync();
+            }
+        }
+
+        private void RemoveQuotes()
+        {
+            for (int i = QuotesViewer.Children.Count - 2; i >= 0; i--)
+            {
+                QuotesViewer.Children.RemoveAt(i);
+                QuotesViewerSnapped.Children.RemoveAt(i);
+            }
+        }
+
+
+        private void MoarLove_Click(object sender, RoutedEventArgs e)
+        {
+            if (QuotesSelector.SelectedIndex == 0)
+                LoadNewQuotes(true);
+            else if (QuotesSelector.SelectedIndex == 1)
+            { 
+                RemoveQuotes(); 
+                LoadRandomQuotes();
+            }
+            else if (AbyssSelector.SelectedIndex == 0)
+            {
+                RemoveQuotes();
+                LoadAbyss();
+            }
+        }
+
+
+    }
+}
